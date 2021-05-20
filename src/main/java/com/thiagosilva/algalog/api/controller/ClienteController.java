f19507f453f71ee69bfe8cbac070d99e73b37c7a
Thiago Silva <thyagosousasilva@gmail.com>
2021-05-20 17:16:25 -0300

    - Movendo a lógica das operações com o banco para uma camada repository - Implementando algumas consultas personalizadas

--- src/main/java/com/thiagosilva/algalog/api/controller/ClienteController.java
+++ src/main/java/com/thiagosilva/algalog/api/controller/ClienteController.java
@@ -12,21 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.thiagosilva.algalog.domain.model.Cliente;
 import com.thiagosilva.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
 // Torna a classe identificável para o Spring framework, fazendo  com que os
 // endpoints cadastrados nela fiquem disponíveis
 @RestController
+@AllArgsConstructor
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
-	
-	@Autowired
 	private ClienteRepository clienteRepository;
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findByNomeContaining("ia");
 	}
 
 }
--- src/main/java/com/thiagosilva/algalog/domain/repository/ClienteRepository.java
+++ src/main/java/com/thiagosilva/algalog/domain/repository/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.thiagosilva.algalog.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,13 @@ import com.thiagosilva.algalog.domain.model.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Long>{
 	
+//	Procura um nome com correspondência exata seguindo um padrão no nome
+//	dos métodos, o próprio jpa já se encarrega de fornecer a implementação
+	List<Cliente> findByNome(String nome);
+	
+//	O nome deste método instrui o JPA a implementar um método que encontra re-
+//	gistros com base na entidade Cliente contendo o nome do parâmetro com cor-
+//	espondência não exata (devido ao sufixo Containing, que vai usar o Like do SQL
+//	para realizar a consulta)
+	List<Cliente> findByNomeContaining(String nome);
 }
