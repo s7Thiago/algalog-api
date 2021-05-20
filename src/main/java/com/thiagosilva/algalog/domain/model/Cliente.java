1e24c7a33177df66982a8c574c527008df26810c
Thiago Silva <thyagosousasilva@gmail.com>
2021-05-20 20:11:50 -0300

    Adicionando validação na entidade Cliente com BeanValidation

--- src/main/java/com/thiagosilva/algalog/domain/model/Cliente.java
+++ src/main/java/com/thiagosilva/algalog/domain/model/Cliente.java
@@ -5,7 +5,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -22,11 +25,18 @@ public class Cliente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
+	@Size(max = 60)
 	private String nome;
 	
+	@NotBlank
+	@Size(max = 255)
+	@Email
 	private String email;
 	
 	@Column(name = "fone")
+	@Size(max = 20)
+	@NotBlank
 	private String telefone;	
 
 }
