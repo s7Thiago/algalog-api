85b9493c7c7266ff9e2eacbfe8fe345d36d0cbda
Thiago Silva <thyagosousasilva@gmail.com>
2021-05-20 22:15:16 -0300

    Implementando Exception Handler

---
+++ src/main/java/com/thiagosilva/algalog/api/exceptionhandler/Problema.java
@@ -0,0 +1,25 @@
+package com.thiagosilva.algalog.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Problema {
+	private Integer status;
+	private LocalDateTime dataHora;
+	private String titulo;
+	private List<Campo> campos;
+	
+	@Getter
+	@AllArgsConstructor
+	public static class Campo {
+		private String nome;
+		private String mensagem;
+		
+	}
+}
