0871079ee00719184e2e52693ca72257cf7d2778
Thiago Silva <thyagosousasilva@gmail.com>
2021-08-10 21:47:26 -0300

    - Implementando exceção para quando o cliente não existir ao solicitar uma entrega - Resolvido problema onde os demais dados além do id do cliente eram retornados todos nulos no json resultante da requisição de solicitaç!ao de entrega

--- src/main/java/com/thiagosilva/algalog/domain/service/SolicitacaoEntregaService.java
+++ src/main/java/com/thiagosilva/algalog/domain/service/SolicitacaoEntregaService.java
@@ -2,8 +2,11 @@ package com.thiagosilva.algalog.domain.service;
 
 import java.time.LocalDateTime;
 
+import com.thiagosilva.algalog.domain.exception.NegocioException;
+import com.thiagosilva.algalog.domain.model.Cliente;
 import com.thiagosilva.algalog.domain.model.Entrega;
 import com.thiagosilva.algalog.domain.model.StatusEntrega;
+import com.thiagosilva.algalog.domain.repository.ClienteRepository;
 import com.thiagosilva.algalog.domain.repository.EntregaRepository;
 
 import org.springframework.stereotype.Service;
@@ -19,6 +22,7 @@ import lombok.AllArgsConstructor;
 @Service
 public class SolicitacaoEntregaService {
 
+    private ClienteRepository clienteRepository;
     private EntregaRepository entregaRepository;
 
     @Transactional
@@ -26,6 +30,14 @@ public class SolicitacaoEntregaService {
         entrega.setStatus(StatusEntrega.PENDENTE); // Quando é feita uma solicitação de entrega, o status é PENDENTE por
                                                    // padrão
 
+        // Verificando se existe o cliente com o id passado nos dados da entrega para
+        // retornar a mensagem adequada caso o mesmo não exista
+        Cliente cliente = clienteRepository.findById(entrega.getCliente().getId())
+                .orElseThrow(() -> new NegocioException(
+                        "Não foi encontrado um cliente com o código " + entrega.getCliente().getId()));
+
+        entrega.setCliente(cliente); // Resolve o problema de as informações do cliente estarem todas null no json de
+                                     // retorno deste método
         entrega.setDataPedido(LocalDateTime.now());
         return entregaRepository.save(entrega);
     }
