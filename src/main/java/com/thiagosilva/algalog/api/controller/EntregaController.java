bdcf8eb34c299f3342cde9198e5270367c06fcb4
Thiago Silva <thyagosousasilva@gmail.com>
2021-08-12 17:03:17 -0300

    Implementando listagem e busca de uma única entrega

--- src/main/java/com/thiagosilva/algalog/api/controller/EntregaController.java
+++ src/main/java/com/thiagosilva/algalog/api/controller/EntregaController.java
@@ -1,11 +1,17 @@
 package com.thiagosilva.algalog.api.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import com.thiagosilva.algalog.domain.model.Entrega;
+import com.thiagosilva.algalog.domain.repository.EntregaRepository;
 import com.thiagosilva.algalog.domain.service.SolicitacaoEntregaService;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +25,7 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/entregas")
 public class EntregaController {
 
+    private EntregaRepository entregaRepository;
     private SolicitacaoEntregaService solicitacaoEntregaService;
 
     @PostMapping
@@ -26,5 +33,22 @@ public class EntregaController {
                                         // por isso a escolha de retornar o status 201
     public Entrega solicitar(@Valid @RequestBody Entrega entrega) {
         return solicitacaoEntregaService.solicitarEntrega(entrega);
+    }
+
+    @GetMapping
+    public List<Entrega> listar() {
+        return entregaRepository.findAll();
+    }
+
+    @GetMapping("/{entregaId}")
+    public ResponseEntity<Entrega> buscar(@PathVariable Long entregaId) {
+        return entregaRepository.findById(entregaId).map(ResponseEntity::ok) // Caso exista alguma coisa dentro deste
+                                                                             // Optional, será retornado
+                                                                             // ResponseEntity.ok com a própria entrega
+                                                                             // que estiver dentro desse Optional como
+                                                                             // corpo da resposta
+                .orElse(ResponseEntity.notFound().build()); // Cai no orElse se não existir nada dentro do Optional,
+                                                            // retornado pelo findById, dessa forma, retornando 404 (Not
+                                                            // Found)
     }
 }
